03e03c23d6671d9c064257a2d9820a5530362edd
Tiago Cabral <tiagorc.cabral@gmail.com>
2021-06-14 17:46:06 -0300

    :sparkles: feat: adds make trade service and logic

--- src/modules/trade/controller/trade-controller.ts
+++ src/modules/trade/controller/trade-controller.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from 'express'
 import httpStatus from 'http-status'
 import { expressRouteAdapter } from '@/main/adapters'
+import { checkTrade } from '@/modules/trade/services'
 // import env from '@/main/config/env'
 
 const makeTrade = expressRouteAdapter(async (req: Request, res: Response) => {
-  res.status(httpStatus.CREATED).send({})
+  const reqData = req.body
+  const tradeOk = await checkTrade(reqData)
+  if (tradeOk) {
+    // await saveTradeHistory()
+    return res.status(httpStatus.CREATED).send({ message: 'Trade done!' })
+  }
+  return res.status(httpStatus.NOT_ACCEPTABLE).send({ message: 'Trade is unfair!' })
 })
 
 export default {
---
+++ src/modules/trade/services/index.ts
@@ -0,0 +1,48 @@
+import httpStatus from 'http-status'
+import axios from 'axios'
+import ApiError from '@/main/utils/apiError'
+
+type PokemonData = {
+  id: number
+  base_experience: number
+}
+
+const requestPokemonData = async (pokemonIds: number[]): Promise<PokemonData[]> => {
+  const responseData: PokemonData[] = []
+  await Promise.all(pokemonIds.map(async id => {
+    try {
+      const data = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      responseData.push({ id: id, base_experience: data.data.base_experience })
+    } catch (error) {
+      throw new ApiError(httpStatus[500], error)
+    }
+  }))
+  return responseData
+}
+
+const checkTrade = async (data): Promise<Boolean> => {
+  try {
+    const { pokemon_ids: pokemons1 } = data.trader1
+    const { pokemon_ids: pokemons2 } = data.trader2
+    let trader1Pokemons: PokemonData[] = []
+    let trader2Pokemons: PokemonData[] = []
+
+    trader1Pokemons = await requestPokemonData(pokemons1)
+    trader2Pokemons = await requestPokemonData(pokemons2)
+
+    const scoreCalculator = (acc: number, currentValue: PokemonData): number => {
+      return acc + currentValue.base_experience
+    }
+
+    const pokemons1Score = trader1Pokemons.reduce(scoreCalculator, 0)
+    const pokemons2Score = trader2Pokemons.reduce(scoreCalculator, 0)
+
+    if (pokemons1Score === pokemons2Score) return true
+    if (Math.max(pokemons1Score, pokemons2Score) * 0.8 >= Math.min(pokemons1Score, pokemons2Score)) return false
+  } catch (error) {
+    throw new ApiError(httpStatus[500], error)
+  }
+  return true
+}
+
+export { checkTrade }
